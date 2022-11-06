943861b12e1c73caa90832c24ec77b38f178cc31
sycho09 <wyswid@naver.com>
2022-11-06 17:33:16 +0900

    chore: setup common constant variables

---
+++ src/utils/constants.js
@@ -0,0 +1,20 @@
+export const CARD_LABEL = {
+  status: '상태',
+  startDate: '광고 생성일',
+  budget: '일 희망 예산',
+  roas: '광고 수익률',
+  convValue: '매출',
+  cost: '광고 비용'
+};
+
+export const FILTER_LIST = {
+  cost: '광고비용',
+  roas: '광고수익률',
+  convValue: '매출',
+  click: '클릭수',
+  imp: '노출수',
+  conv: '전환수'
+};
+
+export const FilterListKeys = Object.keys(FILTER_LIST);
+export const FilterListValues = Object.values(FILTER_LIST);
