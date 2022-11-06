a0d99dec9ba023ebf1b73357e24f221623ac6635
sycho09 <wyswid@naver.com>
2022-11-06 19:07:26 +0900

    feat: chart display option setup function added

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-template */
+/* eslint-disable object-shorthand */
 export function makeSumList(array, start, end) {
   return array
     .slice(start, end)
@@ -13,12 +15,54 @@ export function makeSumList(array, start, end) {
 }
 
 export function getWeeklyData(array, start, end) {
-  const _selected = array.slice(start, end);
+  const selected = array.slice(start, end);
   const temp = {};
-  _selected.forEach((obj) =>
+  selected.forEach((obj) =>
     Object.entries(obj).forEach(([key, val]) => {
       temp[key] ? temp[key].push(val) : (temp[key] = [val]);
     })
   );
   return temp;
+}
+
+export function setOptions(firstFilter, secondFilter) {
+  const options = {
+    responsive: true,
+    interaction: {
+      mode: 'index',
+      intersect: false
+    },
+    stacked: false,
+    scales: {
+      y: {
+        type: 'linear',
+        display: true,
+        position: 'left',
+        ticks: {
+          callback: function (value, index, ticks) {
+            if (firstFilter === '광고비용' || '매출') return value + '원';
+            if (firstFilter === '광고수익률') return value + '%';
+            return value + '회';
+          }
+        }
+      },
+      y1: {
+        type: 'linear',
+        display: true,
+        position: 'right',
+        grid: {
+          drawOnChartArea: false
+        },
+        ticks: {
+          callback: function (value, index, ticks) {
+            if (secondFilter === '광고비용' || '매출') return value + '원';
+            if (secondFilter === '광고수익률') return value + '%';
+            return value + '회';
+          }
+        }
+      }
+    }
+  };
+
+  return options;
 }
