486793f8fabb0189ee9b428d65656faa759fc6aa
sycho09 <wyswid@naver.com>
2022-11-06 19:06:09 +0900

    fix: undefined list error

--- src/molecules/StatusOverview.jsx
+++ src/molecules/StatusOverview.jsx
@@ -11,13 +11,12 @@ export default function StatusOverview() {
   return (
     <Grid container mt={4}>
       {Object.entries(FILTER_LIST).map(([key, val]) => (
-        <CardItemWrapper my={1.5} py={1}>
+        <CardItemWrapper key={val} my={1.5} py={1}>
           <Typography variant='caption'>{[val]}</Typography>
           <Stack direction='row' justifyContent='space-between'>
             <Typography variant='body1'>
-              {selectedData[key]?.toFixed(0)}
+              {selectedData[key] && selectedData[key].toFixed(0)}
             </Typography>
-            <Typography variant='body2'>loading...</Typography>
           </Stack>
         </CardItemWrapper>
       ))}
