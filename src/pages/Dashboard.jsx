f255d2e27ddbab73c138b55af8fd04c3cb2ff2ab
sycho09 <wyswid@naver.com>
2022-11-06 19:07:57 +0900

    feat: update dashboard - add chart and datepicker

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-
 import { Box, Card, CardContent, Grid, Stack, Typography } from '@mui/material';
 import FilterContainer from '../molecules/FilterContainer';
 import StatusOverview from '../molecules/StatusOverview';
+import WeeklyPicker from '../molecules/WeeklyPicker';
+import Chart from '../molecules/Chart';
 
 export default function Dashboard() {
   return (
@@ -11,9 +12,11 @@ export default function Dashboard() {
         <Typography variant='h6' mb={2} sx={{ fontWeight: 'bold' }}>
           광고현황
         </Typography>
+        <WeeklyPicker />
       </Stack>
       <Card>
         <StatusOverview />
+        <Chart />
       </Card>
       <FilterContainer />
     </>
