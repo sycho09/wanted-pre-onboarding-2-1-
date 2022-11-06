12d7232c5c5869fc5cc532ece5e1db3cbab46668
sycho09 <wyswid@naver.com>
2022-11-06 17:47:15 +0900

    feat: add filter container into dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+
 import { Box, Card, CardContent, Grid, Stack, Typography } from '@mui/material';
+import FilterContainer from '../molecules/FilterContainer';
 
 export default function Dashboard() {
   return (
-    <Stack direction='row' justifyContent='space-between' alignItems='center'>
-      <Typography variant='h6' mb={2} sx={{ fontWeight: 'bold' }}>
-        광고현황
-      </Typography>
-    </Stack>
+    <>
+      <Stack direction='row' justifyContent='space-between' alignItems='center'>
+        <Typography variant='h6' mb={2} sx={{ fontWeight: 'bold' }}>
+          광고현황
+        </Typography>
+      </Stack>
+
+      <FilterContainer />
+    </>
   );
 }
