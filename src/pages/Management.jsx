1da57fd85df8d76d30df2d711d89edb637c38d3b
sycho09 <wyswid@naver.com>
2022-11-06 19:29:34 +0900

    feat: management page setup

--- src/pages/Management.jsx
+++ src/pages/Management.jsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Typography } from '@mui/material';
+import AdCard from '../molecules/AdCard';
+import adDataset from '../utils/mockup/adDataset.json';
+import AdStatus from '../utils/type';
 
 export default function Management() {
-  return <div>Management</div>;
+  const [adList, setAdList] = useState([]);
+
+  const makeStatusCards = () => {
+    const { ads: list } = adDataset;
+    const newList = list.map((status) => new AdStatus(status));
+    setAdList(() => newList);
+  };
+
+  useEffect(() => {
+    makeStatusCards();
+  }, []);
+
+  return (
+    <>
+      <Typography variant='h6' mb={2} sx={{ fontWeight: 'bold' }}>
+        광고관리
+      </Typography>
+      {adList && <AdCard list={adList} />}
+    </>
+  );
 }
